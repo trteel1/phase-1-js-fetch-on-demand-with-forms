31530463717a53ac0227d288b7639aeb91d8d0c6
trteel1 <tateelamino@gmail.com>
2023-01-26 20:16:12 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const init = () => {
-  
-}
+const form = document.querySelector('form')
+const formInput = document.querySelector('input#searchByID');
+	const init = () => {
 
-document.addEventListener('DOMContentLoaded', init);
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:5500/movies/${formInput.value}`)
+        .then(response => response.json())
+        .then((data) => {
+            const title = document.querySelector('section#movieDetails h4')
+            const sectionSummary = document.querySelector('section#movieDetails p')
+            title.innerText = data.title
+            sectionSummary.innerText = data.summary
+        })
+    })
+}	
+
+
+document.addEventListener('DOMContentLoaded', init);	
